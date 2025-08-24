9c05720746430f14e6127dbf95ce2274799f58e6
Matheus Pimenta <matheuscscp@gmail.com>
2025-08-24 14:31:43 +0100

    Move API to its own file
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- api.go
+++ api.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/oauth2"
+)
+
 const (
 	// Ping endpoint. Will respond WWW-Authenticate header if a valid bearer token is not provided.
 	pathAuthenticate = "/authenticate"
@@ -12,3 +21,175 @@ const (
 	pathCallback                 = "/callback"
 	pathToken                    = "/token"
 )
+
+func newAPI(p provider, conf *config, sessionStore *sessionStore) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(pathAuthenticate, func(w http.ResponseWriter, r *http.Request) {
+		l := fromRequest(r)
+
+		token := bearerToken(r)
+		if token == "" {
+			respondWWWAuthenticate(w, r)
+			return
+		}
+
+		if err := p.verifyBearerToken(r.Context(), token); err != nil {
+			l.WithError(err).Debug("failed to verify bearer token")
+			respondWWWAuthenticate(w, r)
+			return
+		}
+
+		l.Debug("request authenticated")
+	})
+
+	mux.HandleFunc(pathOAuthProtectedResource, func(w http.ResponseWriter, r *http.Request) {
+		respondJSON(w, r, http.StatusOK, map[string]any{
+			"authorization_servers": []map[string]any{{
+				"issuer":                 baseURL(r),
+				"authorization_endpoint": fmt.Sprintf("%s%s", baseURL(r), pathAuthorize),
+			}},
+		})
+	})
+
+	mux.HandleFunc(pathOAuthAuthorizationServer, func(w http.ResponseWriter, r *http.Request) {
+		respondJSON(w, r, http.StatusOK, map[string]any{
+			"issuer":                                baseURL(r),
+			"authorization_endpoint":                fmt.Sprintf("%s%s", baseURL(r), pathAuthorize),
+			"token_endpoint":                        fmt.Sprintf("%s%s", baseURL(r), pathToken),
+			"registration_endpoint":                 fmt.Sprintf("%s%s", baseURL(r), pathRegister),
+			"scopes_supported":                      p.supportedScopes(),
+			"token_endpoint_auth_methods_supported": []string{authorizationServerAuthMethod},
+			"response_types_supported":              []string{authorizationServerResponseType},
+			"response_modes_supported":              []string{authorizationServerResponseMode},
+			"grant_types_supported":                 []string{authorizationServerGrantType},
+			"code_challenge_methods_supported":      []string{authorizationServerCodeChallengeMethod},
+		})
+	})
+
+	mux.HandleFunc(pathRegister, func(w http.ResponseWriter, r *http.Request) {
+		var m map[string]any
+		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+			fromRequest(r).WithError(err).Error("failed to parse request body as JSON")
+			http.Error(w, "Failed to parse request body as JSON", http.StatusBadRequest)
+			return
+		}
+		redirectURIs, ok := m["redirect_uris"]
+		if !ok {
+			http.Error(w, "Missing redirect_uris", http.StatusBadRequest)
+			return
+		}
+		respondJSON(w, r, http.StatusCreated, map[string]any{
+			"client_id":                  conf.Provider.ClientID,
+			"token_endpoint_auth_method": authorizationServerAuthMethod,
+			"redirect_uris":              redirectURIs,
+		})
+	})
+
+	mux.HandleFunc(pathAuthorize, func(w http.ResponseWriter, r *http.Request) {
+		l := fromRequest(r)
+
+		if r.URL.Query().Get(queryParamCodeChallengeMethod) != authorizationServerCodeChallengeMethod {
+			http.Error(w, "Unsupported code_challenge_method", http.StatusBadRequest)
+			return
+		}
+
+		codeVerifier, err := pkceVerifier()
+		if err != nil {
+			l.WithError(err).Error("failed to generate code verifier")
+			http.Error(w, "Failed to generate code verifier", http.StatusInternalServerError)
+			return
+		}
+		codeChallenge := pkceS256Challenge(codeVerifier)
+
+		tx := r.URL.Query()
+		tx.Set(queryParamCodeVerifier, codeVerifier)
+		state, err := sessionStore.store(tx, nil)
+		if err != nil {
+			l.WithError(err).Error("failed to generate state")
+			http.Error(w, "Failed to generate state", http.StatusInternalServerError)
+			return
+		}
+		setState(w, r, state)
+
+		url := p.oauth2Config(r).AuthCodeURL(state,
+			oauth2.SetAuthURLParam(queryParamCodeChallenge, codeChallenge),
+			oauth2.SetAuthURLParam(queryParamCodeChallengeMethod, authorizationServerCodeChallengeMethod))
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	})
+
+	mux.HandleFunc(pathCallback, func(w http.ResponseWriter, r *http.Request) {
+		l := fromRequest(r)
+
+		state, err := getAndDeleteStateAndCheckCSRF(w, r)
+		if err != nil {
+			l.WithError(err).Error("failed to check CSRF")
+			http.Error(w, "Failed to check CSRF", http.StatusBadRequest)
+			return
+		}
+
+		tx, _, ok := sessionStore.retrieve(state)
+		if !ok {
+			http.Error(w, "Session expired", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := p.oauth2Config(r).Exchange(r.Context(), authorizationCode(r),
+			oauth2.SetAuthURLParam(queryParamCodeVerifier, tx.Get(queryParamCodeVerifier)))
+		if err != nil {
+			l.WithError(err).Error("failed to exchange authorization code for tokens")
+			http.Error(w, "Failed to exchange authorization code for tokens", http.StatusBadRequest)
+			return
+		}
+
+		tokens, err := p.verifyAndRepackExchangedTokens(r.Context(), oauth2Token)
+		if err != nil {
+			l.WithError(err).Error("failed to verify user")
+			http.Error(w, "Failed to verify user", http.StatusBadRequest)
+			return
+		}
+
+		authzCode, err := sessionStore.store(tx, tokens)
+		if err != nil {
+			l.WithError(err).Error("failed to store tokens with authorization code")
+			http.Error(w, "Failed to store tokens with authorization code", http.StatusInternalServerError)
+			return
+		}
+
+		redirectURI := tx.Get(queryParamRedirectURI)
+		redirectParams := url.Values{}
+		redirectParams.Set(queryParamAuthorizationCode, authzCode)
+		redirectParams.Set(queryParamState, tx.Get(queryParamState))
+		redirectURL := fmt.Sprintf("%s?%s", redirectURI, redirectParams.Encode())
+
+		http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+	})
+
+	mux.HandleFunc(pathToken, func(w http.ResponseWriter, r *http.Request) {
+		l := fromRequest(r)
+
+		if err := r.ParseForm(); err != nil {
+			l.WithError(err).Error("failed to parse form")
+			http.Error(w, "Failed to parse form", http.StatusBadRequest)
+			return
+		}
+
+		authzCode := r.FormValue(queryParamAuthorizationCode)
+		tx, tokens, ok := sessionStore.retrieve(authzCode)
+		if !ok {
+			http.Error(w, "Authorization code expired", http.StatusBadRequest)
+			return
+		}
+
+		codeChallenge := tx.Get(queryParamCodeChallenge)
+		codeVerifier := r.FormValue(queryParamCodeVerifier)
+		if codeChallenge != pkceS256Challenge(codeVerifier) {
+			http.Error(w, "PKCE failed", http.StatusBadRequest)
+			return
+		}
+
+		respondJSON(w, r, http.StatusOK, tokens)
+	})
+
+	return mux
+}
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,7 +14,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/oauth2"
 )
 
 func getProviderAndConfig() (provider, *config) {
@@ -40,184 +37,14 @@ func main() {
 	signal.Notify(signalReceived, os.Interrupt, syscall.SIGTERM)
 
 	p, conf := getProviderAndConfig()
-
-	sessionStore := newSessionStore()
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc(pathAuthenticate, func(w http.ResponseWriter, r *http.Request) {
-		l := fromRequest(r)
-
-		token := bearerToken(r)
-		if token == "" {
-			respondWWWAuthenticate(w, r)
-			return
-		}
-
-		if err := p.verifyBearerToken(r.Context(), token); err != nil {
-			l.WithError(err).Debug("failed to verify bearer token")
-			respondWWWAuthenticate(w, r)
-			return
-		}
-
-		l.Debug("request authenticated")
-	})
-
-	mux.HandleFunc(pathOAuthProtectedResource, func(w http.ResponseWriter, r *http.Request) {
-		respondJSON(w, r, http.StatusOK, map[string]any{
-			"authorization_servers": []map[string]any{{
-				"issuer":                 baseURL(r),
-				"authorization_endpoint": fmt.Sprintf("%s%s", baseURL(r), pathAuthorize),
-			}},
-		})
-	})
-
-	mux.HandleFunc(pathOAuthAuthorizationServer, func(w http.ResponseWriter, r *http.Request) {
-		respondJSON(w, r, http.StatusOK, map[string]any{
-			"issuer":                                baseURL(r),
-			"authorization_endpoint":                fmt.Sprintf("%s%s", baseURL(r), pathAuthorize),
-			"token_endpoint":                        fmt.Sprintf("%s%s", baseURL(r), pathToken),
-			"registration_endpoint":                 fmt.Sprintf("%s%s", baseURL(r), pathRegister),
-			"scopes_supported":                      p.supportedScopes(),
-			"token_endpoint_auth_methods_supported": []string{authorizationServerAuthMethod},
-			"response_types_supported":              []string{authorizationServerResponseType},
-			"response_modes_supported":              []string{authorizationServerResponseMode},
-			"grant_types_supported":                 []string{authorizationServerGrantType},
-			"code_challenge_methods_supported":      []string{authorizationServerCodeChallengeMethod},
-		})
-	})
-
-	mux.HandleFunc(pathRegister, func(w http.ResponseWriter, r *http.Request) {
-		var m map[string]any
-		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
-			fromRequest(r).WithError(err).Error("failed to parse request body as JSON")
-			http.Error(w, "Failed to parse request body as JSON", http.StatusBadRequest)
-			return
-		}
-		redirectURIs, ok := m["redirect_uris"]
-		if !ok {
-			http.Error(w, "Missing redirect_uris", http.StatusBadRequest)
-			return
-		}
-		respondJSON(w, r, http.StatusCreated, map[string]any{
-			"client_id":                  conf.Provider.ClientID,
-			"token_endpoint_auth_method": authorizationServerAuthMethod,
-			"redirect_uris":              redirectURIs,
-		})
-	})
-
-	mux.HandleFunc(pathAuthorize, func(w http.ResponseWriter, r *http.Request) {
-		l := fromRequest(r)
-
-		if r.URL.Query().Get(queryParamCodeChallengeMethod) != authorizationServerCodeChallengeMethod {
-			http.Error(w, "Unsupported code_challenge_method", http.StatusBadRequest)
-			return
-		}
-
-		codeVerifier, err := pkceVerifier()
-		if err != nil {
-			l.WithError(err).Error("failed to generate code verifier")
-			http.Error(w, "Failed to generate code verifier", http.StatusInternalServerError)
-			return
-		}
-		codeChallenge := pkceS256Challenge(codeVerifier)
-
-		tx := r.URL.Query()
-		tx.Set(queryParamCodeVerifier, codeVerifier)
-		state, err := sessionStore.store(tx, nil)
-		if err != nil {
-			l.WithError(err).Error("failed to generate state")
-			http.Error(w, "Failed to generate state", http.StatusInternalServerError)
-			return
-		}
-		setState(w, r, state)
-
-		url := p.oauth2Config(r).AuthCodeURL(state,
-			oauth2.SetAuthURLParam(queryParamCodeChallenge, codeChallenge),
-			oauth2.SetAuthURLParam(queryParamCodeChallengeMethod, authorizationServerCodeChallengeMethod))
-		http.Redirect(w, r, url, http.StatusSeeOther)
-	})
-
-	mux.HandleFunc(pathCallback, func(w http.ResponseWriter, r *http.Request) {
-		l := fromRequest(r)
-
-		state, err := getAndDeleteStateAndCheckCSRF(w, r)
-		if err != nil {
-			l.WithError(err).Error("failed to check CSRF")
-			http.Error(w, "Failed to check CSRF", http.StatusBadRequest)
-			return
-		}
-
-		tx, _, ok := sessionStore.retrieve(state)
-		if !ok {
-			http.Error(w, "Session expired", http.StatusBadRequest)
-			return
-		}
-
-		oauth2Token, err := p.oauth2Config(r).Exchange(r.Context(), authorizationCode(r),
-			oauth2.SetAuthURLParam(queryParamCodeVerifier, tx.Get(queryParamCodeVerifier)))
-		if err != nil {
-			l.WithError(err).Error("failed to exchange authorization code for tokens")
-			http.Error(w, "Failed to exchange authorization code for tokens", http.StatusBadRequest)
-			return
-		}
-
-		tokens, err := p.verifyAndRepackExchangedTokens(r.Context(), oauth2Token)
-		if err != nil {
-			l.WithError(err).Error("failed to verify user")
-			http.Error(w, "Failed to verify user", http.StatusBadRequest)
-			return
-		}
-
-		authzCode, err := sessionStore.store(tx, tokens)
-		if err != nil {
-			l.WithError(err).Error("failed to store tokens with authorization code")
-			http.Error(w, "Failed to store tokens with authorization code", http.StatusInternalServerError)
-			return
-		}
-
-		redirectURI := tx.Get(queryParamRedirectURI)
-		redirectParams := url.Values{}
-		redirectParams.Set(queryParamAuthorizationCode, authzCode)
-		redirectParams.Set(queryParamState, tx.Get(queryParamState))
-		redirectURL := fmt.Sprintf("%s?%s", redirectURI, redirectParams.Encode())
-
-		http.Redirect(w, r, redirectURL, http.StatusSeeOther)
-	})
-
-	mux.HandleFunc(pathToken, func(w http.ResponseWriter, r *http.Request) {
-		l := fromRequest(r)
-
-		if err := r.ParseForm(); err != nil {
-			l.WithError(err).Error("failed to parse form")
-			http.Error(w, "Failed to parse form", http.StatusBadRequest)
-			return
-		}
-
-		authzCode := r.FormValue(queryParamAuthorizationCode)
-		tx, tokens, ok := sessionStore.retrieve(authzCode)
-		if !ok {
-			http.Error(w, "Authorization code expired", http.StatusBadRequest)
-			return
-		}
-
-		codeChallenge := tx.Get(queryParamCodeChallenge)
-		codeVerifier := r.FormValue(queryParamCodeVerifier)
-		if codeChallenge != pkceS256Challenge(codeVerifier) {
-			http.Error(w, "PKCE failed", http.StatusBadRequest)
-			return
-		}
-
-		respondJSON(w, r, http.StatusOK, tokens)
-	})
+	api := newAPI(p, conf, newSessionStore())
 
 	addr := conf.Server.Addr
 	if addr == "" {
 		addr = ":8080"
 	}
-	handler := http.Handler(mux)
 	if conf.Server.CORS {
-		handler = handleCORS(handler)
+		api = handleCORS(api)
 	}
 	promHandler := promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{
 		EnableOpenMetrics: true,
@@ -251,7 +78,7 @@ func main() {
 			case "/metrics":
 				promHandler.ServeHTTP(w, r)
 			default:
-				handler.ServeHTTP(w, r)
+				api.ServeHTTP(w, r)
 			}
 		}),
 	}
